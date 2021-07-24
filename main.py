d21ef2d2886a6fa962446436c55457ed3d5d0429
aqib.javed <aqibbutt3078@gmail.com>
2021-07-24 03:40:36 +0200

    function, local variable , global variable and redefining global inside a function, deleting a variable

--- main.py
+++ main.py
@@ -11,14 +11,26 @@ def setfunction():
     print(s.upper() + str(f).join(" sdhjgdj"))
 
 
+def add(val1, val2=0):
+    return val1 + val2
+
+
+def mul(va1, val2=1):
+    return va1*val2
+
+
 def main():
-    setfunction()
-    print(f)
+    print(mul(22, 2))
+    print(add(1))
+    # print(setfunction is setfunction)
+    # print(setfunction() is None)
+    # setfunction()
+    # print(f)
     # del f
-    temp = -2
-    print(temp)
+    # temp = -2
+    # print(temp)
     # del temp
-    print(temp)
+    # print(temp)
     # print(f) f is deleted
     # name = input("What is your name ?")
     # print(name)
